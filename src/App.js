fb3f3fbe769ed883580c7cd87d84002f142a5435
noobCode-69 <armanyppph@gmail.com>
2024-08-18 00:20:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        border: "1px solid black",
+        padding: "1rem",
+        width: "fit-content",
+      }}
+    >
+      <div>Current count : {count}</div>
+      <button onClick={() => setCount(count + 1)}>Add</button>
+      <button onClick={() => setCount(count - 1)}>Remove</button>
+      {!data.userId ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          <div>{data.title}</div>
+        </div>
+      )}
     </div>
   );
 }
